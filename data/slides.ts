8848feadb284e39827c02851f1be51f9bbde71ae
Ruslan Menibaev <ruslanmenibaev@gmail.com>
2020-09-21 00:22:51 +0200

    initial slider layout

---
+++ data/slides.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: "1",
+    key: "first",
+    type: 'Some Type',
+    heading: 'Some heading',
+    description: "Lorem ipsum dolor sit amet.",
+    color: "#A0ECD0"
+  },
+  {
+    id: "2",
+    key: "two",
+    type: 'Some Type 2',
+    heading: 'Some heading 2',
+    description: "Lorem ipsum dolor sit amet.",
+    color: "#A0ECD0"
+  },
+  {
+    id: "3",
+    key: "three",
+    type: 'Some Type 3',
+    heading: 'Some heading 3',
+    description: "Lorem ipsum dolor sit amet.",
+    color: "#A0ECD0"
+  }
+]
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,92 @@
+import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { StyleSheet, FlatList, Dimensions } from 'react-native';
 import { Text, View } from '../components/Themed';
+import slides from '../data/slides';
+
+const { width, height } = Dimensions.get('window');
+const LOGO_WIDTH = 240;
+const LOGO_HEIGHT = 80;
+const DOT_SIZE = 40;
+
+type ItemProps = {
+  heading: string
+  description: string
+}
+
+const Item: React.FC<ItemProps> = ({ heading, description }) => {
+  return (
+    <View style={styles.itemStyle}>
+      <View style={styles.textContainer}>
+        <Text style={styles.heading}>{heading}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    </View>
+  )
+}
+
+const Pagination = () => <View style={styles.pagination} />
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <StatusBar style="auto" hidden />
+      <FlatList
+        data={slides}
+        keyExtractor={item => item.key}
+        renderItem={({ item, index }) => <Item {...item} />}
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+      <Pagination />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  itemStyle: {
+    width,
+    height,
+    alignItems: "center",
+    justifyContent: "center"
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  // imageStyle: {
+  //   width: width * 0.75,
+  //   height: width * 0.75,
+  //   resizeMode: "contain",
+  //   flex: 1
+  // },
+  textContainer: {
+    alignItems: "flex-start",
+    alignSelf: "flex-end",
+    flex: 0.5,
+    backgroundColor: "#ccc"
   },
+  heading: {
+    textTransform: "uppercase",
+    fontWeight: "800",
+    fontSize: 24,
+    letterSpacing: 2,
+    marginBottom: 5
+  },
+  description: {
+    width: width * 0.75,
+    marginRight: 10,
+    fontSize: 16,
+    lineHeight: 16 * 1.5
+  },
+
+  pagination: {
+    position: "absolute",
+    right: 40,
+    bottom: 35,
+    backgroundColor: "#fd130e",
+    flexDirection: "row",
+    height: DOT_SIZE,
+    width: 50
+  }
 });
