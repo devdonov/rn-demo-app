841a610df0f83e422927953ef1d50d1c5eff18b8
Ruslan Menibaev <ruslanmenibaev@gmail.com>
2020-09-24 21:08:06 +0200

    Update posters data.

--- data/posters.ts
+++ data/posters.ts
@@ -8,15 +8,22 @@ export type Poster = {
 
 export default [
   {
-    url: "",
+    url: "https://www.creative-flyers.com/wp-content/uploads/2017/07/halloween-flyer-template.jpg",
     key: "1",
     title: "Lorem, ipsum dolor.",
     location: "New York",
     date: "Monday"
   },
   {
-    url: "",
+    url: "https://www.creative-flyers.com/wp-content/uploads/2019/10/Minimalist-Halloween-Flyer.jpg",
     key: "2",
+    title: "Lorem, ipsum dolor.",
+    location: "New York",
+    date: "Monday"
+  },
+  {
+    url: "https://www.creative-flyers.com/wp-content/uploads/2019/09/Halloween-party-flyer.jpg",
+    key: "3",
     title: "Lorem, ipsum dolor.",
     location: "New York",
     date: "Monday"
